8fd9fa35ffe291186f4d7bd95322f08f4bf36317
martin-martin <breuss.martin@gmail.com>
2017-08-20 23:26:36 +0200

    feat: recreate component styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,33 @@ import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  },
+  {
+    "id": "martin",
+    "name": "Martin Breuss",
+    "email": "martin-not@reacttraining.com",
+    "avatarURL": "https://avatars0.githubusercontent.com/u/12371494?v=4&u=ade0bf6c16ed2313397019c7a5a9d0d8eedff245&s=400"
+  }
+]
+
 class App extends Component {
   state = {
     contacts: []
@@ -32,20 +59,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            onDeleteContact={this.removeContact}
-            contacts={this.state.contacts}
-          />
-        )}/>
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )}/>
+        Life is good.
+        <ListContacts contacts={contacts}/>
       </div>
     )
   }
