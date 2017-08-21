7de3f999a7208194244a080a1ef3f949e495f9c7
martin-martin <breuss.martin@gmail.com>
2017-08-21 22:37:16 +0200

    feat: initialize state

--- src/App.js
+++ src/App.js
@@ -4,63 +4,41 @@ import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  },
-  {
-    "id": "martin",
-    "name": "Martin Breuss",
-    "email": "martin-not@reacttraining.com",
-    "avatarURL": "https://avatars0.githubusercontent.com/u/12371494?v=4&u=ade0bf6c16ed2313397019c7a5a9d0d8eedff245&s=400"
-  }
-]
-
 class App extends Component {
   state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
-  }
-  removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      },
+      {
+        "id": "martin",
+        "name": "Martin Breuss",
+        "email": "martin-not@reacttraining.com",
+        "avatarURL": "https://avatars0.githubusercontent.com/u/12371494?v=4&u=ade0bf6c16ed2313397019c7a5a9d0d8eedff245&s=400"
+      }
+    ]
   }
 
   render() {
     return (
       <div>
         Life is good.
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     )
   }
