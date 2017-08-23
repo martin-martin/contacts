6a800217469da1ca73118d3f1885edf05f996a3b
martin-martin <breuss.martin@gmail.com>
2017-08-23 18:45:51 +0200

    refactor: object destructuring

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -21,14 +21,16 @@ class ListContacts extends Component {
   }
 
   render() {
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
 
     let showingContacts
 
-    if (this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
     } else {
-      showingContacts = this.props.contacts
+      showingContacts = contacts
     }
 
     showingContacts.sort(sortBy('name'))
@@ -40,7 +42,7 @@ class ListContacts extends Component {
             className='search-contacts'
             type='text'
             placeholder='Search Contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
@@ -54,7 +56,7 @@ class ListContacts extends Component {
                   <p>{contact.name}</p>
                   <p>{contact.email}</p>
                 </div>
-                <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                   Remove
                 </button>
               </li>
