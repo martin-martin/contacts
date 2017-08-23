c434759a5986e1883141a1e2a847424eff0f495e
martin-martin <breuss.martin@gmail.com>
2017-08-23 18:30:15 +0200

    feat: add controlled componet for filtering contacts

--- src/App.js
+++ src/App.js
@@ -34,17 +34,19 @@ class App extends Component {
     ]
   }
 
-removeContact = (contact) => {
-  this.setState((state) => ({
-    contacts: state.contacts.filter((c) => c.id != contact.id)
-  }))
-}
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id != contact.id)
+    }))
+  }
 
   render() {
     return (
       <div>
         Life is good.
-        <ListContacts contacts={this.state.contacts}/>
+        <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}/>
       </div>
     )
   }
