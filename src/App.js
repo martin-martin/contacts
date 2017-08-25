06f5a2341cddf7d787e7adc7ee3c816f3d437384
martin-martin <breuss.martin@gmail.com>
2017-08-25 17:42:49 +0200

    feat: add page-flippin' feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'create', // 'list', 'create'
+    screen: 'list', // 'list', 'create'
     contacts: []
   }
 
@@ -29,8 +29,11 @@ class App extends Component {
       <div className='app'>
         {this.state.screen === 'list' && (
           <ListContacts
-            onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }}
           />
         )}
         {this.state.screen === 'create' && (
