bc65f6abf3bae8bda652293e62030a0bed14283b
martin-martin <breuss.martin@gmail.com>
2017-08-25 17:09:09 +0200

    feat: communicate to server

--- src/App.js
+++ src/App.js
@@ -6,38 +6,21 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      },
-      {
-        "id": "martin",
-        "name": "Martin Breuss",
-        "email": "martin-not@reacttraining.com",
-        "avatarURL": "https://avatars0.githubusercontent.com/u/12371494?v=4&u=ade0bf6c16ed2313397019c7a5a9d0d8eedff245&s=400"
-      }
-    ]
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
   }
 
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id != contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
 
   render() {
